7f7c1ee9e75e45e4c04759b86e11d15b8a382d30
Arthur <46850965+arthur-rn@users.noreply.github.com>
2026-06-22 21:19:50 -0300

    Adicionado o código do projeto

---
+++ CaronaAmiga/src/main/java/br/edu/iff/ccc/caronaamiga/CaronaamigaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.caronaamiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaronaamigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaronaamigaApplication.class, args);
+	}
+
+}
---
+++ CaronaAmiga/src/test/java/br/edu/iff/ccc/caronaamiga/CaronaamigaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.caronaamiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaronaamigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
